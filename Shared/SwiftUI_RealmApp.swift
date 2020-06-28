399a01661642640255a14b64e600472b001ba044
ShawnyNicole <shawnynicole@gmail.com>
2020-06-28 16:02:45 -0500

    Initial Commit

---
+++ Shared/SwiftUI_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RealmApp.swift
+//  Shared
+//
+//  Created by DeShawn Jackson on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
